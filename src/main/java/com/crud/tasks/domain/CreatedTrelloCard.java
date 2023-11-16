18e91c497d70ded917502373d37c1c2c59d0fbb6
RafalNicki <Nicol79@op.pl>
2023-11-16 19:19:57 +0100

    [22.4] java 17

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -2,10 +2,15 @@ package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreatedTrelloCard {
     @JsonProperty("id")
@@ -16,4 +21,10 @@ public class CreatedTrelloCard {
     private String shortUrl;
     @JsonProperty("badges")
     private Map<String, Object> badges;
+
+    public CreatedTrelloCard(String id, String name, String shortUrl) {
+        this.id = id;
+        this.name = name;
+        this.shortUrl = shortUrl;
+    }
 }
--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -3,19 +3,18 @@ package com.crud.tasks.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
+
+@SuppressWarnings("ALL")
 @Component
-@Data
+@AllArgsConstructor
 @RequiredArgsConstructor
+@Data
 public class TrelloCardDto {
     private String name;
     private String description;
     private String pos;
     private String listId;
-
-
 
 }
